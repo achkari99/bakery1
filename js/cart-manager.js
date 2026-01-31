1b09b06e14046b607ab8a28526d5a1972c0086b9
achkari99 <mohamed.achkaribegdouri1@etu.uae.ac.ma>
2026-01-31 19:02:03 +0100

    phone version

--- js/cart-manager.js
+++ js/cart-manager.js
@@ -207,7 +207,7 @@ class CartManager {
                         <circle cx="20" cy="21" r="1"></circle>
                         <path d="M1 1h4l2.68 13.39a2 2 0 0 0 2 1.61h9.72a2 2 0 0 0 2-1.61L23 6H6"></path>
                     </svg>
-                    <p>Votre panier est vide</p>
+                    <p data-i18n="cart.empty_title">${(window.I18n && window.I18n.t) ? window.I18n.t('cart.empty_title', 'Your cart is empty') : 'Your cart is empty'}</p>
                 </div>
             `;
             return;
